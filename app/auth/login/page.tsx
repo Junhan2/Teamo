8cf6d9d96c4f711735683b01f2d9df1ad16c8bf4
leegangjoon2 <leegangjoon2@gmail.com>
2025-05-26 15:54:47 +0900

    refine: reduce particle size for more delicate effect
    - Change particle size from 1-11px to 0.5-2.5px
    - Update spread parameter from 10 to 2
    - Create more subtle and elegant particle animation
    - Maintain 400 particle count for density while reducing visual noise

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -53,7 +53,7 @@ export default function LoginPage() {
         mouseInteraction={true}
         particleTransparency={true}
         baseSize={100}
-        spread={10}
+        spread={2}
         speed={0.1}
         className="opacity-40"
       />
